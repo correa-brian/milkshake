dedb2c25cb284d684f1352ced2d361d1c21747dc
Katrina Rodriguez <karodriguez8@gmail.com>
2016-05-30 20:31:08 -0400

    add search bar to homeVC, map annotations from foursquare search results, push view to createGameVC from pins

---
+++ milkshake/Models/MSVenues.swift
@@ -0,0 +1,13 @@
+//
+//  MSVenues.swift
+//  milkshake
+//
+//  Created by Katrina Rodriguez on 5/30/16.
+//  Copyright © 2016 milkshake-systems. All rights reserved.
+//
+
+import UIKit
+
+class MSVenues: NSObject {
+
+}
