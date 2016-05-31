57bd37ab8cf2315e6ef093c84e8d77cd33015923
Katrina Rodriguez <karodriguez8@gmail.com>
2016-05-30 20:32:04 -0400

    search bar, search result map annotations, and push to createGameVC from map pin

--- milkshake/Models/MSVenues.swift
+++ milkshake/Models/MSVenues.swift
@@ -7,7 +7,50 @@
 //
 
 import UIKit
+import MapKit
 
-class MSVenues: NSObject {
+class MSVenues: NSObject, MKAnnotation {
+    
+    var name: String?
+    var address: String?
+    var latitude: Double?
+    var longitude: Double?
+    
+    //MARK: Required Protocol Methods for MK Annotation
+    var title: String? {
+        return self.name
+    }
+    
+    var subtitle: String? {
+        return self.address
+    }
+    
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2DMake(self.latitude!, self.longitude!)
+    }
+    
+    //MARK: Parsing Function
+    func populate(venueInfo: Dictionary<String, AnyObject>){
+        
+        if let _name = venueInfo["name"] as? String {
+            self.name = _name
+        }
+        
+        if let location = venueInfo["location"] as? Dictionary<String, AnyObject>{
+            if let _address = location["address"] as? String {
+                self.address = _address
+            }
+        
+        if let lat = location["lat"] as? Double {
+            self.latitude = lat
+        }
+        
+        if let lng = location["lng"] as? Double {
+            self.longitude = lng
+        }
+        
+            print("- - - - - - POPULATE: \(self.name), \(self.address) - - - - - - - - ")
 
+        }
+    }
 }
--- milkshake/ViewControllers/MSHomeViewController.swift
+++ milkshake/ViewControllers/MSHomeViewController.swift
@@ -11,28 +11,61 @@ import CoreLocation
 import MapKit
 import Alamofire
 
-class MSHomeViewController: MSViewController, CLLocationManagerDelegate, MKMapViewDelegate {
+class MSHomeViewController: MSViewController, CLLocationManagerDelegate, MKMapViewDelegate, UITextFieldDelegate {
 
     //MARK: Properties
-    
     var mapView: MKMapView!
+    var bottomView: UIView!
+    var searchField: UITextField!
     var locationManager: CLLocationManager!
     var allGames = Array<MSGame>()
+    var venues = Array<MSVenues>()
+    
     
     //MARK: Lifecyle Methods
-    
     override func loadView() {
         let frame = UIScreen.mainScreen().bounds
         let view = UIView(frame: frame)
         view.backgroundColor = UIColor(red: 255/255, green: 44/255, blue: 0/255, alpha: 1)
         
         self.mapView = MKMapView(frame: frame)
-        
         self.mapView.showsUserLocation = true
         self.mapView.delegate = self
         view.addSubview(self.mapView)
         
         self.view = view
+        
+        let height = CGFloat(44)
+        let width = frame.size.width
+        let y = frame.size.height
+        self.bottomView = UIView(frame: CGRect(x: 0, y: y, width: width, height: height))
+        self.bottomView.autoresizingMask = .FlexibleTopMargin
+//        self.bottomView.backgroundColor = UIColor.blackColor()
+        view.addSubview(bottomView)
+        
+        let padding = CGFloat(6)
+        let btnWidth = CGFloat(80)
+        
+        self.searchField = UITextField(frame: CGRect(x: padding, y: padding, width: width-2*padding-btnWidth, height: height-2*padding))
+        self.searchField.borderStyle = .RoundedRect
+        self.searchField.layer.borderColor = UIColor.darkGrayColor().CGColor
+        self.searchField.placeholder = "search activities nearby"
+        self.searchField.delegate = self
+        self.bottomView.addSubview(self.searchField)
+        
+        let btnSearch = UIButton(type: .Custom)
+        btnSearch.frame = CGRect(x: width-btnWidth, y: padding, width: 74, height: height-2*padding)
+        btnSearch.setTitle("Search", forState: .Normal)
+        btnSearch.backgroundColor = UIColor.darkGrayColor()
+        btnSearch.layer.cornerRadius = 5
+        btnSearch.layer.masksToBounds = true
+        btnSearch.layer.borderColor = UIColor.darkGrayColor().CGColor
+        btnSearch.layer.borderWidth = 0.5
+        self.bottomView.addSubview(btnSearch)
+        
+        let action = #selector(MSHomeViewController.searchVenues(_:))
+        btnSearch.addTarget(self, action: action, forControlEvents: .TouchUpInside)
+        
     }
     
     override func viewDidLoad() {
@@ -47,7 +80,6 @@ class MSHomeViewController: MSViewController, CLLocationManagerDelegate, MKMapVi
     }
     
     // MARK: - LocationManagerDelegate
-    
     func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus){
         if(status == .AuthorizedWhenInUse){
             self.locationManager.startUpdatingLocation()
@@ -73,7 +105,7 @@ class MSHomeViewController: MSViewController, CLLocationManagerDelegate, MKMapVi
         }
         
         self.locationManager.stopUpdatingLocation()
-        print("Found Current Location: \(loc.description)")
+//        print("Found Current Location: \(loc.description)")
         self.locationManager.delegate = nil
         
         let center = CLLocationCoordinate2DMake(loc.coordinate.latitude, loc.coordinate.longitude)
@@ -83,53 +115,92 @@ class MSHomeViewController: MSViewController, CLLocationManagerDelegate, MKMapVi
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(center, regionRadius, regionRadius)
         self.mapView.setRegion(coordinateRegion, animated: true)
         
-        let latLng = "\(loc.coordinate.latitude),\(loc.coordinate.longitude)"
-        let url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=\(latLng)&radius=1000&type=park&key=AIzaSyCwljFOsMKmipbjQyDvZ0kAEf9zb_RTbtc"
+        UIView.animateWithDuration(0.4, animations: {
+            var bottomFrame = self.bottomView.frame
+            bottomFrame.origin.y = bottomFrame.origin.y-self.bottomView.frame.size.height
+            
+            self.bottomView.frame = bottomFrame
+        })
+
+    }
+    
+    //MARK: - TextField Delegate
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("textFieldShouldReturn: ")
+        searchField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: - My Functions
+    func searchVenues(btn: UIButton){
+        let searchText = self.searchField.text!
+        if (searchText.characters.count == 0){
+            let msg = "Hm we missed that. Try again please. "
+            let alert = UIAlertController(title: "Typo", message: msg, preferredStyle: .Alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .Default, handler: nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+            return
+        }
         
-        Alamofire.request(.GET, url, parameters: nil).responseJSON { response in
-            if let JSON = response.result.value as? Dictionary<String, AnyObject>{
-                if let results = JSON["results"] as? Array<Dictionary<String, AnyObject>>{
+        let currentLocation = self.mapView.centerCoordinate
+        let latLng = "\(currentLocation.latitude), \(currentLocation.longitude)"
+        
+        let url = "https://api.foursquare.com/v2/venues/search"
+        let params = [
+            "v": "20140806",
+            "ll": latLng,
+            "query": searchText,
+            "client_id": "L3H5O451ES1YRHBZF12AG45YYY3X0NOTY2FBMZU5EBMAFR42",
+            "client_secret": "X0LF5CX1RLDSCD3HLYQCMOD1IB2XV2UM5NUGSGYZVCZTHTZR"
+        ]
+        
+        Alamofire.request(.GET, url, parameters: params).responseJSON { response in
+           if let json = response.result.value as? Dictionary<String, AnyObject>{
+            if let resp = json["response"] as? Dictionary<String, AnyObject>{
+                if let venuesArray = resp["venues"] as? Array<Dictionary<String, AnyObject>>{
+                  
+                    self.mapView.removeAnnotations(self.venues)
+                    self.venues.removeAll()
                     
-                    dispatch_async(dispatch_get_main_queue(), {
-                        for parkInfo in results {
-                            let park = MSGame()
-                            park.populate(parkInfo)
-                            self.allGames.append(park)
-                        }
-                        
-                        self.mapView.addAnnotations(self.allGames)
-                    })
-                    
+                    for venueInfo in venuesArray {
+                        let venue = MSVenues()
+                        venue.populate(venueInfo)
+                        self.venues.append(venue)
+                    }
+                        self.mapView.addAnnotations(self.venues)
                 }
             }
         }
     }
-    
-    //MARK: - MapViewDelgate
-    
-    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        if let annotation = annotation as? MSGame {
-            let identifier = "pin"
-            
-            if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) as? MKPinAnnotationView {
-                dequeuedView.annotation = annotation
-                dequeuedView.canShowCallout = true
-                return dequeuedView
-            }
-        }
-        
-        return nil
-    }
-    
-    //MARK: - My Functions
+}
     
     func createGame(btn: UIBarButtonItem){
         print("createGame")
         
         let createGameVc = MSCreateProfileViewController()
         presentViewController(createGameVc, animated: true, completion: nil)
+    }
+    
+    //MARK: - MapViewDelegate
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+    
+        let annotation = annotation as? MSVenues
+        let identifier = "pin"
         
+        if let dequeuedView = mapView.dequeueReusableAnnotationViewWithIdentifier(identifier) as? MKPinAnnotationView{
+            return dequeuedView
+        }
+        
+        let pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+        pinView.annotation = annotation
+        pinView.canShowCallout = true
+        pinView.animatesDrop = true
+        return pinView
+    }
+    
+    func mapView(mapView: MKMapView, didSelectAnnotationView view: MKAnnotationView){
+        let createGameVC = MSCreateGameViewController()
+        navigationController?.pushViewController(createGameVC, animated: true)
     }
     
     override func didReceiveMemoryWarning() {
